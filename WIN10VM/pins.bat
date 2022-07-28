ea1af933d56fead12aa7a636e3527fc35a923f31
Jérémie Tarot (silopolis) <silopolis@gmail.com>
2022-07-28 16:49:30 +0200

    WIN10VM: Initial commit

---
+++ WIN10VM/pins.bat
@@ -0,0 +1,6 @@
+@echo off
+rem pinned packages found by choco-package-list-backup.ps1 on 2022-06-08 :
+echo   ** Re-pinning previously pinned Chocolatey packages.
+choco pin add -n=kicad
+choco pin add -n=openjdk
+choco pin add -n=python3
